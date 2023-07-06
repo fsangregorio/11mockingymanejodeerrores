b8b8cf62ca7431fb278a3728995381a9a8b0c743
fsangregorio <103077244+fsangregorio@users.noreply.github.com>
2023-07-06 17:08:38 -0300

    Add files via upload

---
+++ src/presentation/application/appExpress.js
@@ -0,0 +1,35 @@
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import productRouter from "../routes/productsRoute.js";
+import sessionRouter from "../routes/sessionsRoute.js";
+import roleRouter from "../routes/roleRoute.js";
+import cartRouter from "../routes/cartsRoute.js";
+import userRouter from "../routes/usersRoute.js";
+import errorHandler from "../middleware/errorHandler.js";
+import logger from "../middleware/logger.js";
+
+class AppExpress {
+  init() {
+    this.app = express();
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieParser());
+  }
+  build() {
+    this.app.use(logger);
+    this.app.use("/api/sessions", sessionRouter);
+    this.app.use("/api/users", userRouter);
+    this.app.use("/api/roles", roleRouter);
+    this.app.use("/api/products", productRouter);
+    this.app.use("/api/carts", cartRouter);
+    this.app.use(errorHandler);
+  }
+  listen() {
+    return this.app.listen(process.env.PORT, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
+    });
+  }
+}
+
+export default AppExpress;
